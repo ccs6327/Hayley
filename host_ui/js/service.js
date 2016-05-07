17a2ac14e15531b12ad19f142da49d7f8faf1df1
Jack Ong <jackong2013@gmail.com>
2016-05-07 15:21:33 -0700

    Revert "Initial commit of the UI with add new user function"
    This reverts commit aa35315a79c9969cc19ef3e98decb661610e57d9.

--- host_ui/js/service.js
+++
@@ -1,9 +0,0 @@
-angular.module('hayleyService', [])
-	.factory('dbServices', function ($http) {
-		return {
-			addUser : function (userDetail) {
-				console.log(userDetail);
-				return $http.post('/apis/addUser', userDetail);
-			}
-		}
-	});
